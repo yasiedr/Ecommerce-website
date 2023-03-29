cf8c797f10886261750e005e0838c06f90f4c78d
yasedrisian <yasedrisian-KnNIvq>
2023-03-29 17:55:41 -0400

    getRoutescommit

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,26 +3,86 @@ const { Tag, Product, ProductTag } = require('../..');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/',async (req, res) => {
+  try {
+    const TagData = await Tag.findAll({
+      attribute:["id",'tag_Id'],
+      include: [{ model: Product }],
+    });
+    res.status(200).json(Tag);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // find all tags
   // be sure to include its associated Product data
 });
 
 router.get('/:id', (req, res) => {
+  router.get('/:id', async (req, res) => {
+    try {
+      const libraryCardData = await LibraryCard.findByPk(req.params.id, {
+        include: [{ model: Reader }],
+      });
+  
+      if (!libraryCardData) {
+        res.status(404).json({ message: 'No library card found with that id!' });
+        return;
+      }
+  
+      res.status(200).json(libraryCardData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+  // CREATE a card
+ 
+  
+  // DELETE a card
+
+  
+  
+  
   // find a single tag by its `id`
   // be sure to include its associated Product data
 });
 
-router.post('/', (req, res) => {
+
   // create a new tag
-});
 
-router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
-});
 
-router.delete('/:id', (req, res) => {
+  router.post('/', async (req, res) => {
+    try {
+      const TagData = await Tag.create({
+        Tag_id: req.body,
+      });
+      res.status(200).json(TagData);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+
+
+  router.delete('/:id', async (req, res) => {
+    try {
+      const ProductData = await Tag.destroy({
+        where: {
+          id: req.params.id,
+        },
+      });
+      if (Tag) {
+        res.status(404).json({ message: '' });
+        return;
+      }
+  
+      res.status(200).json();
+    } catch (err) {
+      res.status(500).json(err);
+    }
+
+
   // delete on tag by its `id` value
 });
+
 
 module.exports = router;

52ee3fc8607ff78977c2e39c7caf28061106eb3a
yasedrisian <yasedrisian-KnNIvq>
2023-04-02 21:41:28 -0400

    fixing routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,6 +6,19 @@ const { Category, Product } = require('../..');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    attributes: ['id', 'category_name'],
+    // be sure to include its associated Products
+    include: [{
+      model: Product,
+      attributes: ['id', 'product_name', 'price', 'stock', "category_id"]
+    }]
+  })
+  then(dbCategoryData => res.json(dbCategoryData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.get('/:id', (req, res) => {
@@ -17,12 +30,46 @@ router.post('/', (req, res) => {
   // create a new category
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+router.put('/:id', (req, res) => {Category.update(
+  {
+    category_name: req.body.category_name
+  },
+  {
+    where: {
+      id: req.params.id
+    }
+  }
+)
+  .then(dbCategoryData => {
+    if (!dbCategoryData) {
+      res.status(404).json({ message: 'No matching ctegory available' });
+      return;
+    }
+    res.json(dbCategoryData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
+  // update a category by its `id` value
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbCategoryData => {
+
+      res.json(dbCategoryData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err)
+    });
 });
 
 module.exports = router;
